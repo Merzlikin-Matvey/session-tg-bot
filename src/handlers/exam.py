1455cb4ba99cbf789fbb75ea9768fb9477688061
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-12-24 22:10:46 +0300

    поменял запросы на консультацию

--- src/handlers/exam.py
+++ src/handlers/exam.py
@@ -2,13 +2,14 @@ from datetime import datetime
 
 from aiogram import types, Router, Bot
 from aiogram.fsm.context import FSMContext
-
+import os
+from dotenv import load_dotenv
 from src.database.db_adapter import DatabaseAdapter
 from src.objects.exam import Exam
 from src.objects.user import User
 from src.forms import Form
 from src.keyboards.user_keyboards import *
-
+load_dotenv()
 router = Router()
 adapter = DatabaseAdapter()
 
@@ -62,29 +63,6 @@ async def leave_exam(callback_query: types.CallbackQuery):
     user.set_registered_exam(None)
     await message.edit_text(f"Вы успешно покинули экзамен {exam.name}", reply_markup=user_main_menu_keyboard)
 
-
-@router.callback_query(lambda c: c.data == 'request_consultation')
-async def handle_ready_to_answer(callback_query: types.CallbackQuery, state: FSMContext):
-    await callback_query.answer()
-    telegram_id = callback_query.from_user.id
-    message = callback_query.message
-    user = User(telegram_id)
-    exam = Exam.get_exam_by_id(user.get_registered_exam())
-    if exam:
-        if exam.examiners:
-            response = "Список доступных экзаменаторов:\n"
-            for idx, examiner_id in enumerate(exam.examiners, start=1):
-                examiner = User(examiner_id)
-                response += f"{idx}. {examiner.name}\n"
-            response += "Введите номер экзаменатора для запроса консультации:"
-            await message.edit_text(response)
-            await state.set_state(Form.awaiting_examiner_number)
-        else:
-            await message.edit_text("Нет доступных экзаменаторов для консультации.", reply_markup=user_exam_keyboard)
-    else:
-        await message.edit_text("Экзамен не найден.", reply_markup=user_exam_keyboard)
-
-
 async def send_consultation_request(bot: Bot, examiner_id, requester_id, requester_name):
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=[[
         types.InlineKeyboardButton(text="Принять", callback_data=f"accept_consultation:{requester_id}"),
@@ -98,24 +76,25 @@ async def send_consultation_request(bot: Bot, examiner_id, requester_id, request
     )
 
 
-@router.message(Form.awaiting_examiner_number)
-async def process_examiner_number(message: types.Message, state: FSMContext):
-    try:
-        examiner_number = int(message.text)
-        print(f"Выбранный номер экзаменатора: {examiner_number}")
-        telegram_id = message.from_user.id
-        user = User(telegram_id)
-        exam = Exam.get_exam_by_id(user.get_registered_exam())
-        if 1 <= examiner_number <= len(exam.examiners):
-            examiner_id = exam.examiners[examiner_number - 1]
-            await send_consultation_request(message.bot, examiner_id, message.from_user.id, message.from_user.full_name)
-            await message.answer("Запрос на консультацию отправлен экзаменатору.")
-            await state.clear()
+@router.callback_query(lambda c: c.data == 'request_consultation')
+async def handle_ready_to_answer(callback_query: types.CallbackQuery, state: FSMContext):
+    await callback_query.answer()
+    telegram_id = callback_query.from_user.id
+    message = callback_query.message
+    user = User(telegram_id)
+    exam = Exam.get_exam_by_id(user.get_registered_exam())
+    bot = Bot(str(os.getenv("BOT_API_TOKEN")))
+    if exam:
+        if exam.examiners:
+            await message.edit_text("Вы запросили консультацию, ждите.", reply_markup=user_exam_keyboard)
+            for i in exam.examiners:
+                await send_consultation_request(bot, i, telegram_id, callback_query.from_user.full_name)
         else:
-            await message.answer("Неверный номер экзаменатора. Пожалуйста, попробуйте снова.")
-    except ValueError as e:
-        print(e)
-        await message.answer("Пожалуйста, введите корректный номер экзаменатора.")
+            await message.edit_text("Нет доступных экзаменаторов для консультации.", reply_markup=user_exam_keyboard)
+    else:
+        await message.edit_text("Экзамен не найден.", reply_markup=user_exam_keyboard)
+
+
 
 
 @router.callback_query(lambda c: c.data.startswith("accept_consultation"))
@@ -193,7 +172,7 @@ async def send_ready_notification(bot: Bot, examiner_id, student_id, student_nam
     return message.message_id
 
 
-@router.message(lambda message: message.text == "Готов отвечать")
+@router.callback_query(lambda c: c.data == "student_ready")
 async def ready_to_answer_command(message: types.Message, state: FSMContext):
     telegram_id = message.from_user.id
     user = User(telegram_id)
@@ -213,3 +192,4 @@ async def ready_to_answer_command(message: types.Message, state: FSMContext):
             await message.reply("Вы не зарегистрированы на экзамен.")
     else:
         await message.reply("Вы не зарегистрированы в системе.")
+
