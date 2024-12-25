344c64a2b7fef4258f9e34a8eebf8707b800022c
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-12-25 18:06:13 +0300

    фиксим

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -3,6 +3,7 @@ import sys
 import time
 sys.path.append('./')
 from src.objects.user import User
+from src.database.db_adapter import DatabaseAdapter
 
 
 class UserTests(unittest.TestCase):
@@ -24,7 +25,19 @@ class UserTests(unittest.TestCase):
     def test_get_all_exams(self):
         try:
             user = User(5064226866)
+            adapter = DatabaseAdapter()
+            adapter.add_exam("test","2052-12-25 10:23:00")
+            user.set_registered_exam(1)
             exams = user.get_all_exams()
+            self.assertIsNotNone(exams)
+        except Exception as e:
+            self.fail(f"Ошибка: {e}")
+
+
+    def test_get_registered_exams(self):
+        try:
+            user = User(5064226866)
+            exams = user.get_registered_exams()
             self.assertIsNotNone(exams)
         except Exception as e:
             self.fail(f"Ошибка: {e}")
